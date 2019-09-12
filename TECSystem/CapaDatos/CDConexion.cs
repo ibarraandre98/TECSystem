0f70415df4342c58f670a8f5a73ca2a34d10aee0
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-09-12 15:51:38 -0500

    Agregado de cadena de conexión a la BD

--- TECSystem/CapaDatos/CDConexion.cs
+++ TECSystem/CapaDatos/CDConexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,18 @@ namespace CapaDatos
 {
     public class CDConexion
     {
-
+        private SqlConnection Conexion = new SqlConnection("Data Source=.;Initial Catalog=BDTEC;Integrated Security=True");
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == System.Data.ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == System.Data.ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
     }
 }
