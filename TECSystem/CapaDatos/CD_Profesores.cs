fb58fe631bdc1f69f0d7657aa4ec9d9847f02bce
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-09-13 18:25:08 -0500

    profesores

--- TECSystem/CapaDatos/CD_Profesores.cs
+++ TECSystem/CapaDatos/CD_Profesores.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CapaDatos
 {
     public class CD_Profesores
     {
+        CDConexion conexion = new CDConexion();
+        SqlDataReader leer;
+        SqlCommand comando = new SqlCommand();
+        DataTable TablaProfesores = new DataTable();
 
+        public DataTable MostrarProfesores()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "select * from Profesores";
+            comando.CommandType = CommandType.Text;
+            leer = comando.ExecuteReader();
+            TablaProfesores.Load(leer);
+            conexion.CerrarConexion();
+            return TablaProfesores;
+        }
+
+        public void InsertarProfesores()
+        {
+
+        }
     }
 }
