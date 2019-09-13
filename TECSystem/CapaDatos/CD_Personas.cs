4f5ad2d12d2e8398d2b8454bb4e2280fd7060bd6
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-09-13 00:29:28 -0500

    Agregado de personas

--- TECSystem/CapaDatos/CD_Personas.cs
+++ TECSystem/CapaDatos/CD_Personas.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CapaDatos
 {
     class CD_Personas
     {
-        public void AgregarPersonas()
+        CDConexion conexion = new CDConexion();
+        SqlDataReader leer;
+        DataTable TablaPersonas = new DataTable();
+        SqlCommand comando = new SqlCommand();
+        public DataTable MostrarPersonas()
         {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "select * from personas";
 
+            return TablaPersonas;
         }
 
     }
