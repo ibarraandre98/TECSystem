c1e87e1166119cbf74ca045f5494c12b5916209f
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-09-13 01:28:57 -0500

    Clase Persona

--- TECSystem/CapaDatos/CD_Personas.cs
+++ TECSystem/CapaDatos/CD_Personas.cs
@@ -18,9 +18,35 @@ namespace CapaDatos
         {
             comando.Connection = conexion.AbrirConexion();
             comando.CommandText = "select * from personas";
-
+            comando.CommandType = CommandType.Text;
+            leer = comando.ExecuteReader();
+            TablaPersonas.Load(leer);
+            conexion.CerrarConexion();
             return TablaPersonas;
         }
 
+        public void AgregarPersonas(String paterno, String materno, String nombres, String fecha_nac, int sexo, String curp, String telefono, int idCalle, String numExt, String numInt, String cp, int edoCivil, int discapacidad)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "insert into personas" +
+                "(paterno, materno, nombres,fecha_nac, sexo, curp, telefono, idCalle, numExt, numInt, cp, edoCivil, discapacidad) " +
+                "values('"+paterno+"', '"+materno+ "', '"+nombres+ "', '"+fecha_nac+ "', "+sexo+ ", '"+curp+ "', '"+telefono+ "', "+idCalle+ ", '"+numExt+ "', '"+numInt+ "', '"+cp+ "', "+edoCivil+ ", "+discapacidad+");";
+            comando.CommandType = CommandType.Text;
+            comando.ExecuteNonQuery();
+        }
+
+        public void EditarPersonas(int idPersona, String paterno, String materno, String nombres, String fecha_nac, int sexo, String curp, String telefono, int idCalle, String numExt, String numInt, String cp, int edoCivil, int discapacidad)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "update personas set paterno = '"+paterno+ "', materno = '"+materno+ "', nombres = '"+nombres+ "',fecha_nac = '"+fecha_nac+ "', sexo = "+sexo+ ", curp = '"+curp+ "', telefono = '"+telefono+ "', idCalle = "+idCalle+ ", numExt = '"+numExt+ "', numInt = '"+numInt+ "', cp = '"+cp+ "', edoCivil = '"+edoCivil+ "', discapacidad = '"+discapacidad+ "' where idPersona = '"+idPersona+"';";
+            comando.CommandType = CommandType.Text;
+            comando.ExecuteNonQuery();
+        }
+
+        public void EliminarPersona(int idPersona)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "delete from persona where idPersona = '"+idPersona+"';";
+        }
     }
 }
