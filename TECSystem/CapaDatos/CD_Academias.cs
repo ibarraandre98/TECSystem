c3cfb6e9cc0461421276a84787254945a3c8f492
MIGUELGM98 <gonzalez.martinez.16040@itsmante.edu.mx>
2019-09-16 22:18:32 -0500

    primer_cambio

---
+++ TECSystem/CapaDatos/CD_Academias.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class CD_Academias{
+        CDConexion conexion = new CDConexion();
+        SqlDataReader leer;
+        DataTable mos= new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable mostrar()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "select * from academias";
+            leer = comando.ExecuteReader();
+            mos.Load(leer);
+            conexion.CerrarConexion();
+            return mos;
+        }
+        public void insertar(string nombre)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "insert into academias values (@nombre)";
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            leer = comando.ExecuteReader();
+            conexion.CerrarConexion();          
+        }
+        public void eliminar(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "delete from academias where idNombre = @idNombre";
+            comando.Parameters.AddWithValue("@idNombre", id);
+            leer = comando.ExecuteReader();
+            conexion.CerrarConexion();
+        }
+    }
+}
---
+++ TECSystem/CapaNegocio/CN_Academia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class CN_Academia
+    {
+        CD_Academias obj = new CD_Academias();
+
+        public DataTable mostrarAcademias()
+        {
+            DataTable tabla = new DataTable();
+            tabla = obj.mostrar();
+            return tabla;
+        }
+        public void agregar_academia(String nombre)
+        {
+            obj.insertar(nombre);
+        }
+    }
+    
+}
