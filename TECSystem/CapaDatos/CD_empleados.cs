af095474534056199863567006fa9c16001b584a
Oscar Rdz <Oscar Rdz@DESKTOP-B91CM40>
2019-09-17 19:05:38 -0500

    clases

---
+++ TECSystem/CapaDatos/CD_empleados.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_empleados
+    {
+        CDConexion conexion = new CDConexion();
+        SqlDataReader leer;
+        DataTable tablaempleados = new DataTable();
+        SqlCommand comando = new SqlCommand();
+        public DataTable Mostrarempleados()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "select * from empleados";
+            comando.CommandType = CommandType.Text;
+            leer = comando.ExecuteReader();
+            tablaempleados.Load(leer);
+            conexion.CerrarConexion();
+            return tablaempleados;
+        }
+
+        public void Agregarempleados(int idEmpleado, int idPersona, int idEmpleo)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "insert into empleados" +
+                "(idEmpleado, idPersona, idEmpleo) " +
+                "values(" + idEmpleado + ", " + idPersona + "" + idEmpleo + ");";
+            comando.CommandType = CommandType.Text;
+            comando.ExecuteNonQuery();
+        }
+
+        public void Editarempleados(int idEmpleado, int idPersona, int idEmpleo)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "update empleados set idEmpleado = " + idEmpleado + ", idPersona = " + idPersona + ", idEmpleo = " + idEmpleo + "';";
+            comando.CommandType = CommandType.Text;
+            comando.ExecuteNonQuery();
+        }
+
+        public void Eliminarempleados(int idEmpleado)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "delete from empleados where idEmpleado = '" + idEmpleado + "';";
+        }
+    }
+}
---
+++ TECSystem/CapaNegocio/CN_empleados.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    class CN_empleados
+    {
+        CD_empleados _CD_empleados = new CD_empleados();
+        DataTable tablaempleados = new DataTable();
+
+        public DataTable MostrarPersonas()
+        {
+            return tablaempleados = _CD_empleados.Mostrarempleados();
+        }
+
+        public void Agregarempleados(int idEmpleado, int idPersona, int idEmpleo)
+        {
+
+            _CD_empleados.Agregarempleados(Convert.ToInt32(idEmpleado), Convert.ToInt32(idPersona), Convert.ToInt32(idEmpleo));
+        }
+
+        public void Editarempleados(int idEmpleado, int idPersona, int idEmpleo)
+        {
+            _CD_empleados.Editarempleados(Convert.ToInt32(idEmpleado), Convert.ToInt32(idPersona), Convert.ToInt32(idEmpleo));
+        }
+
+        public void Eliminarempleados(int idEmpleado)
+        {
+            _CD_empleados.Eliminarempleados(Convert.ToInt32(idEmpleado));
+        }
+    }
+}
