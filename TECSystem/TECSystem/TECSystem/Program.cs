91a5215612945ccec1b7e8e09b3b4be2bcd70e14
Macoy <marco_97@live.com.mx>
2019-11-24 17:51:52 -0600

    Moví el program

--- TECSystem/TECSystem/TECSystem/Program.cs
+++ TECSystem/TECSystem/TECSystem/Program.cs
@@ -24,7 +24,7 @@ namespace TECSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Principal());
+            Application.Run(new Login());
         }
     }
 }
